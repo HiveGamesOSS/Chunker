c57b12bc7868df9bbd7738eee8cae2476da154bd
HiveGames-OSS <youri+hivegamesoss@hivemc.com>
2024-11-03 11:19:55 +0000

    1.3.1

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
 }
 
 group = "com.hivemc.chunker"
-version = "1.3.0"
+version = "1.3.1"
 description = "chunker"
 base.archivesName = "chunker-cli"
 java.sourceCompatibility = JavaVersion.VERSION_17

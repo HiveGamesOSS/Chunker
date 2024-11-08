cbce24231287f9cfbe35295ec1a9bdb24a59ea1b
HiveGames-OSS <youri+hivegamesoss@hivemc.com>
2024-11-08 11:33:29 +0000

    Add mapping for "village_blacksmith" LootTable for lower than 1.14 Java

--- cli/src/main/java/com/hivemc/chunker/conversion/encoding/java/base/resolver/blockentity/legacy/handlers/JavaLegacyRandomizableContainerBlockEntityHandler.java
+++ cli/src/main/java/com/hivemc/chunker/conversion/encoding/java/base/resolver/blockentity/legacy/handlers/JavaLegacyRandomizableContainerBlockEntityHandler.java
@@ -2,6 +2,7 @@ package com.hivemc.chunker.conversion.encoding.java.base.resolver.blockentity.le
 
 import com.hivemc.chunker.conversion.encoding.base.resolver.blockentity.BlockEntityHandler;
 import com.hivemc.chunker.conversion.encoding.java.base.resolver.JavaResolvers;
+import com.hivemc.chunker.conversion.encoding.java.base.resolver.blockentity.handlers.JavaRandomizableContainerBlockEntityHandler;
 import com.hivemc.chunker.conversion.intermediate.column.blockentity.container.randomizable.RandomizableContainerBlockEntity;
 import com.hivemc.chunker.nbt.tags.collection.CompoundTag;
 import org.jetbrains.annotations.NotNull;
@@ -17,7 +18,6 @@ public class JavaLegacyRandomizableContainerBlockEntityHandler extends BlockEnti
      */
     public static final Set<String> UNSUPPORTED_LOOT_TABLES = Set.of(
             "minecraft:chests/monster_room",
-            "minecraft:chests/village_blacksmith",
             "minecraft:chests/village_two_room_house"
     );
 
@@ -34,14 +34,27 @@ public class JavaLegacyRandomizableContainerBlockEntityHandler extends BlockEnti
     public void read(@NotNull JavaResolvers resolvers, @NotNull CompoundTag input, @NotNull RandomizableContainerBlockEntity blockEntity) {
         String lootTable = input.getString("LootTable", null);
         if (lootTable != null && !lootTable.isEmpty() && enableLootTables) {
-            blockEntity.setLootTable(lootTable);
+            blockEntity.setLootTable(applyVersionSpecificLootTableFixes(resolvers, lootTable, false));
         }
     }
 
     @Override
     public void write(@NotNull JavaResolvers resolvers, @NotNull CompoundTag output, @NotNull RandomizableContainerBlockEntity blockEntity) {
         if (blockEntity.getLootTable() != null && !blockEntity.getLootTable().isEmpty() && !UNSUPPORTED_LOOT_TABLES.contains(blockEntity.getLootTable()) && enableLootTables) {
-            output.put("LootTable", blockEntity.getLootTable());
+            output.put("LootTable", applyVersionSpecificLootTableFixes(resolvers, blockEntity.getLootTable(), true));
         }
+    }
+
+    /**
+     * Apply any version specific java fixes to loot tables.
+     *
+     * @param resolvers the resolvers being used.
+     * @param lootTable the input loot table.
+     * @param writing   whether it is being read or written (true if written).
+     * @return the output loot table.
+     */
+    public String applyVersionSpecificLootTableFixes(@NotNull JavaResolvers resolvers, String lootTable, boolean writing) {
+        if (!writing) return lootTable; // Currently fixes are only applied on writing to preserve data
+        return JavaRandomizableContainerBlockEntityHandler.PRE_1_14_LOOT_TABLES.getOrDefault(lootTable, lootTable);
     }
 }

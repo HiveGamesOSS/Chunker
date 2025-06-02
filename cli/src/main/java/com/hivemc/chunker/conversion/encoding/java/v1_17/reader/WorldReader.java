f8428feb3ff2cfcf12770abb6a40335dca625c75
HiveGames-OSS <youri+hivegamesoss@hivemc.com>
2025-06-02 10:29:27 +0100

    Ensure combining columns fails if position isn't present in entities

--- cli/src/main/java/com/hivemc/chunker/conversion/encoding/java/v1_17/reader/WorldReader.java
+++ cli/src/main/java/com/hivemc/chunker/conversion/encoding/java/v1_17/reader/WorldReader.java
@@ -33,7 +33,7 @@ public class WorldReader extends com.hivemc.chunker.conversion.encoding.java.v1_
         // If only one element is present it may just be entities
         if (main == null && entities != null) {
             main = new CompoundTag();
-            int[] position = entities.getIntArray("Position");
+            int[] position = entities.getIntArray("Position", null);
             if (position == null) return null; // No valid position
 
             main.put("xPos", position[0]);
